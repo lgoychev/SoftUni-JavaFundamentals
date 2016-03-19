a30cd234e2cd0af9be79333daa2f79379cce3aed
Lubomir Goychev <lgoychev@123-MacBook-Pro.local>
2016-03-19 09:04:39 +0200

    Add Source code of Introduction to Java Homework

---
+++ 01_Introduction-to-Java/Source code/Intro-Java-Homework/src/AssignVariables.java
@@ -0,0 +1,17 @@
+
+
+public class AssignVariables {
+    public static void main(String[] args){
+        byte byteType = 127;
+        short shortType = 32767;
+        int intType = 2000000000;
+        long longType =  919827112351L;
+        char charType = 'c';
+        boolean booleanType = false;
+        float floatType =  0.5f;
+        double doubleType = 0.1234567891011;
+        String stringType = "Palo Alto, CA";
+        short notShortType = 32768; // Try to assign 32768 - max value of short is 32767!!!
+    }
+
+}
