143b10c773d78732f3386936def0896eb1c1e2d7
Lyubomir Goychev <goychev@me.com>
2016-03-20 08:14:39 +0200

    Homework: Introduction to Java
    Add “Problem 5. Print a Character Triangle”

--- 01_Introduction-to-Java/Source code/Intro-Java-Homework/src/AssignVariables.java
+++ 01_Introduction-to-Java/Source code/Intro-Java-Homework/src/AssignVariables.java
@@ -11,7 +11,7 @@ public class AssignVariables {
         float floatType =  0.5f;
         double doubleType = 0.1234567891011;
         String stringType = "Palo Alto, CA";
-        short notShortType = 32768; // Try to assign 32768 - max value of short is 32767!!!
+        //short notShortType = 32768; // Try to assign 32768 - max value of short is 32767!!!
         double doubleType2 = 0.1234;
     }
 
