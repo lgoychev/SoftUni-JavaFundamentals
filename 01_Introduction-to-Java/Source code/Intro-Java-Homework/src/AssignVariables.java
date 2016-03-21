f45e98a308acbb3a3d2fd074bfa14b0c564fd923
Lubomir Goychev <lgoychev@123-MacBook-Pro.local>
2016-03-21 07:31:08 +0200

    Homework: Introduction to Java
    Add Problem 2, Print all Caharacters

--- 01_Introduction-to-Java/Source code/Intro-Java-Homework/src/AssignVariables.java
+++ 01_Introduction-to-Java/Source code/Intro-Java-Homework/src/AssignVariables.java
@@ -1,18 +1,16 @@
 
-
 public class AssignVariables {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         byte byteType = 127;
         short shortType = 32767;
         int intType = 2000000000;
-        long longType =  919827112351L;
+        long longType = 919827112351L;
         char charType = 'c';
         boolean booleanType = false;
-        float floatType =  0.5f;
+        float floatType = 0.5f;
         double doubleType = 0.1234567891011;
         String stringType = "Palo Alto, CA";
-        short notShortType = 32768; // Try to assign 32768 - max value of short is 32767!!!
-        double doubleType2 = 0.1234;
+        //short notShortType = 32768; // Try to assign 32768 - max value of short is 32767!!!
     }
 
 }
---
+++ 01_Introduction-to-Java/Source code/Intro-Java-Homework/src/PrintCharacters.java
@@ -0,0 +1,13 @@
+/**
+ * Created by lgoychev on 3/19/16.
+ */
+public class PrintCharacters {
+    public static void main(String args[])
+    {
+        char i;
+
+        for( i = 'a' ; i <= 'z' ; i++ )
+            System.out.print(" " + i);
+    }
+}
+
