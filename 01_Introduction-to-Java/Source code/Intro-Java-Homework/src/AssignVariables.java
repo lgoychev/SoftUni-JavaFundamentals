1708a3a0f5459f4adc035afd8631c3022deb20af
Lubomir Goychev <lgoychev@123-MacBook-Pro.local>
2016-03-21 07:39:59 +0200

    merge

--- 01_Introduction-to-Java/Source code/Intro-Java-Homework/src/AssignVariables.java
+++ 01_Introduction-to-Java/Source code/Intro-Java-Homework/src/AssignVariables.java
@@ -1,20 +1,26 @@
 
+
 public class AssignVariables {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         byte byteType = 127;
         short shortType = 32767;
         int intType = 2000000000;
-        long longType = 919827112351L;
+        long longType =  919827112351L;
         char charType = 'c';
         boolean booleanType = false;
-        float floatType = 0.5f;
+        float floatType =  0.5f;
         double doubleType = 0.1234567891011;
         String stringType = "Palo Alto, CA";
+<<<<<<< HEAD
         //short notShortType = 32768; // Try to assign 32768 - max value of short is 32767!!!
 <<<<<<< HEAD
 =======
         double doubleType2 = 0.1234;
 >>>>>>> origin/master
+=======
+        short notShortType = 32768; // Try to assign 32768 - max value of short is 32767!!!
+        double doubleType2 = 0.1234;
+>>>>>>> parent of f45e98a... Homework: Introduction to Java
     }
 
 }
